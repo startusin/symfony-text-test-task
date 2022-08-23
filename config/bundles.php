63db98a8a81b649163d6f6dcb0698298468a7a45
Vytaliy Makovyak <thisinfoms@gmail.com>
2022-08-23 04:12:03 +0300

    Oxford Dictionary API client

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
---
+++ src/Client/OxfordDictionaryClient.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Client;
+
+use GuzzleHttp\Client as GuzzleClient;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
+
+class OxfordDictionaryClient extends GuzzleClient
+{
+    public LoggerInterface $logger;
+    public array $env;
+
+    public function __construct(
+        LoggerInterface $logger,
+        array $env,
+        array $config,
+    ) {
+        $this->logger = $logger;
+        $this->env = $env;
+
+        parent::__construct($config);
+    }
+
+    public function request(string $method, $uri = '', array $options = []): ResponseInterface {
+        $request = parent::request($method, $this->env['api_url'] . $uri, $options);
+
+        $this->log('Logging OxfordDictionaryClient ...', [
+            'url' => $this->env['api_url'] . $uri,
+            'method' => $method,
+            'options' => $options,
+            'response' => [
+                'headers' => $request->getHeaders(),
+            ],
+        ]);
+
+        return $request;
+    }
+
+    protected function log(string $text, array $metaData): void {
+        if ($this->env['api_logging']) {
+            $this->logger->info($text, $metaData);
+        }
+    }
+}
